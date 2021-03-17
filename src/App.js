836de74f4e271e32a12ba8651af163572290a172
venkatramanareddy <venkat_ramana@outlook.com>
2021-03-17 23:00:19 +0530

    safe state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import { Background, Parallax } from 'react-parallax';
+// import fox from './images/fox.png'
+import {Helmet} from "react-helmet";
+import Terminal from './Terminal';
+
+let responses = {
+  disclaimer: <div>Welcome to V-shell
+                  <div className="subDisclaimer"><i>type help to know more</i></div>
+              </div>,
+  commandList:[
+    {
+      command: 'ls',
+      response: <div>about.sh 
+                  <br/>interests.sh
+                  <br/>links.sh
+                  </div>
+    },
+    {
+      command: './about.sh',
+      response: <diV>
+                  <h1>Hi, I'm Venkat</h1>
+                  <h3>A full-stack engineer interested in everything CS</h3>
+                </diV>
+    },
+    {
+      command: './interests.sh',
+      response: <h1>namaste</h1>
+    },
+    {
+      command: 'help',
+      response: <div>Here are some of the commands to try out
+                <ul>
+                  <li><b>ls</b> - for list of available commands</li>
+                  <li><b>./about.sh</b> - run a script :-)</li>
+                  <li><b>help</b> - to get back here</li>
+                </ul>
+                There are few hidden easter eggs too! Do drop a star on github repo if you liked this page!</div>
+    }
+  ],
+  colorPallete: ["green","blue","maroon","yellow","aqua","red"],
+  defaultColor: "crimson",
+  error: <div>I'm not sure what info you've asked for. You can ask for help by typing help</div>
+}
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
+      <Helmet>
+        {/* <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script> */}
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap');
+        </style>
+      </Helmet>
+      <Terminal className="terminal"
+                responseList={responses}
+      />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +70,29 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      {/* <Parallax className="mainLayer">
+        <div>
+          <h1>Hi I'm Venkat</h1>
+          <p>
+          A Full-Stack developer with eyes and ears on anything in CS or soccer.
+          </p>
+          <a-scene>
+            <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+            <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+            <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+            <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+            <a-sky color="#BBBBBB"></a-sky>
+          </a-scene>
+        </div>
+      </Parallax>
+      <Parallax bgImageAlt="the fox" 
+      strength={200}
+      className="foxLayer">
+        Content goes here. Parallax height grows with content height.
+        <img src={fox} alt=""
+         className="foxImage"/> 
+      </Parallax> */}
     </div>
   );
 }
---
+++ src/Terminal.jsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import './terminal.css';
+
+const defaultErrorResponse = "Looks like your trying something which I cannot answer, try asking for help";
+let uniqueRowId = 0;    
+
+function getUniqueRowId(){
+    uniqueRowId++;
+    return uniqueRowId;
+}
+
+class Terminal extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.onKeyDown = this.onKeyDown.bind(this);
+        let initialPromptData = [];
+        let initialStyle = {};
+        
+        this.state = {
+            rowData: initialPromptData,
+            promptItemList: initialPromptData.map((element) => this.createRowElement(element)),
+            customStyle: initialStyle
+        }
+    }
+
+    onKeyDown(event){
+        if(event.key === 'Enter'){
+            // Process the command 
+            if(event.target.value == null || event.target.value === ""){
+                return;
+            }
+            if(event.target.value === "clear" || event.target.value === "cls"){
+                this.setState({
+                    rowData: [],
+                    promptItemList: []
+                });
+            }
+            else{
+                // check if we have corresponding command
+                let rowData = this.getCorrespondingCommandOutput(event.target.value);
+                if(rowData != null){
+                    this.updatePrompt(rowData);
+                }
+            }
+            event.preventDefault();
+
+            // cleanup input box
+            event.target.value = "";
+        }
+        if(event.key === 'Tab'){
+            //autocomplete logic
+            if(event.target.value == null || event.target.value === ""){
+                return;
+            }
+            event.preventDefault();
+            let matchingCommand = this.getClosestMatchingCommandName(event.target.value);
+            if( matchingCommand != null ){
+                event.target.value = matchingCommand;
+            }
+        }
+    }
+
+    getClosestMatchingCommandName(partialInput){
+        let resp = partialInput;
+        if(this.props.responseList.commandList != null 
+            && this.props.responseList.commandList.length >= 0){
+                let commandItem = this.props.responseList.commandList.find(o=> o.command.startsWith(partialInput));
+                if(commandItem != null){
+                    resp = commandItem.command
+                }
+        }
+        return resp;
+    }
+
+    getCorrespondingCommandOutput(inputCommand){
+        let rowData = {id: getUniqueRowId()}
+        if(this.props.responseList.commandList != null 
+            && this.props.responseList.commandList.length >= 0){
+
+            let matchingResult = this.props.responseList.commandList.find(o=> o.command === inputCommand)
+            if(matchingResult != null){
+                Object.assign(rowData,{        
+                    type: "generalResponse",
+                    data: matchingResult.response
+                });
+                return rowData;
+            }
+        }
+        
+        if(this.props.responseList.hasOwnProperty('error')){
+            Object.assign(rowData, {        
+                type: "errorResponse",
+                data: this.props.responseList['error'],
+            });
+        }
+        else{
+            Object.assign(rowData, {        
+                type: "errorResponse",
+                data: defaultErrorResponse,
+            });
+        }
+        return rowData;
+    }
+
+    updatePrompt(rowData){
+        let updatedList = this.state.rowData;
+        updatedList.push(rowData);
+        let updatedPromptItemList = this.state.promptItemList;
+        updatedPromptItemList.push(this.createRowElement(rowData));
+        this.setState({
+            rowData: updatedList,
+            promptItemList: updatedPromptItemList
+        });
+    }
+    componentDidMount() {
+        let initialPromptData = [];
+        if (this.props.responseList.disclaimer !=null){
+            initialPromptData.push({
+                type: "disclaimer",
+                data: this.props.responseList.disclaimer
+            });
+        }
+        let initialStyle = {
+            "backgroundColor": "black",
+            "color": "green"
+        };
+        if(this.props.responseList.defaultColor != null){
+            initialStyle.color = this.props.responseList.defaultColor;
+        }
+
+        this.setState({
+            rowData: initialPromptData,
+            promptItemList: initialPromptData.map((element) => this.createRowElement(element)),
+            customStyle: initialStyle
+        });
+
+        this.scrollToBottom();
+    }
+
+    componentDidUpdate() {
+        this.scrollToBottom();
+    }
+
+    scrollToBottom() {
+        if(this.el != null){
+            this.el.scrollIntoView({ behaviour : 'smooth'});
+        }
+    }
+    createRowElement(element){
+        // I know index is not right for using key, but in my case I can live with it
+        if(element.type === "generalResponse" || element.type === "errorResponse"){
+            return <div key={element.id}
+                    ref={el=> {this.el = el;}} 
+                    className="tr tr-general">
+                    <div className="terminalCursor"> {'>>'} </div>
+                    <div>
+                       {element.data} 
+                    </div>
+            </div>
+        }
+        else if(element.type === "disclaimer"){
+            return <div key={element.id} className="tr tr-disclaimer">{element.data}</div>
+        }
+    }
+
+    render(){
+        return(
+            <div className="terminal" style={this.state.customStyle}>
+                <div className="terminalOutputArea"  style={this.state.customStyle}>
+                    {this.state.promptItemList}
+                </div>
+                <div className="terminalInputArea"  style={this.state.customStyle}>
+                    <div className="terminalCursor"  style={this.state.customStyle}> {'>>'} </div>
+                    <input className="terminalInputBox" onSubmit={this.onKeyDown}
+                    onKeyDown={this.onKeyDown} style={this.state.customStyle} autoFocus/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Terminal;
